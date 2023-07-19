7eff9e46723fdeb593225c5299bb19e8b596ee1f
harryjlhu <harryjlhu@tencent.com>
2023-07-19 11:01:43 +0800

    json movifdafdsafdsafds

--- douban_movie.py
+++ douban_movie.py
@@ -71,6 +71,7 @@ def main():
         col_B = 'B%s' % (name.index(i) + 1)
         col_C = 'C%s' % (name.index(i) + 1)
         col_D = 'D%s' % (name.index(i) + 1)
+       # ws1[col_A] = i
         ws1[col_A] = i
         ws1[col_B] = m
         ws1[col_C] = o
